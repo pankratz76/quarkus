0d01c9eb044e6e478212db51c0633f1a2570131a
Vincent Potucek <vincent.potucek@udg.de>
2025-01-07 16:26:44 +0100

    wip

--- core/runtime/src/main/java/io/quarkus/runtime/ApplicationLifecycleManager.java
+++ core/runtime/src/main/java/io/quarkus/runtime/ApplicationLifecycleManager.java
@@ -1,5 +1,23 @@
 package io.quarkus.runtime;
 
+import io.quarkus.bootstrap.logging.InitialConfigurator;
+import io.quarkus.bootstrap.runner.RunnerClassLoader;
+import io.quarkus.runtime.configuration.ConfigurationException;
+import io.quarkus.runtime.graal.DiagnosticPrinter;
+import io.quarkus.runtime.util.ExceptionUtil;
+import io.quarkus.runtime.util.StringUtil;
+import io.smallrye.config.ConfigValidationException;
+import jakarta.enterprise.context.spi.CreationalContext;
+import jakarta.enterprise.inject.Any;
+import jakarta.enterprise.inject.spi.Bean;
+import jakarta.enterprise.inject.spi.BeanManager;
+import jakarta.enterprise.inject.spi.CDI;
+import org.jboss.logging.Logger;
+import org.jboss.logmanager.handlers.AsyncHandler;
+import org.wildfly.common.lock.Locks;
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
 import java.util.List;
 import java.util.Locale;
 import java.util.Set;
@@ -11,25 +29,7 @@ import java.util.logging.ConsoleHandler;
 import java.util.logging.Handler;
 import java.util.stream.Collectors;
 
-import jakarta.enterprise.context.spi.CreationalContext;
-import jakarta.enterprise.inject.Any;
-import jakarta.enterprise.inject.spi.Bean;
-import jakarta.enterprise.inject.spi.BeanManager;
-import jakarta.enterprise.inject.spi.CDI;
-
-import org.jboss.logging.Logger;
-import org.jboss.logmanager.handlers.AsyncHandler;
-import org.wildfly.common.lock.Locks;
-
-import io.quarkus.bootstrap.logging.InitialConfigurator;
-import io.quarkus.bootstrap.runner.RunnerClassLoader;
-import io.quarkus.runtime.configuration.ConfigurationException;
-import io.quarkus.runtime.graal.DiagnosticPrinter;
-import io.quarkus.runtime.util.ExceptionUtil;
-import io.quarkus.runtime.util.StringUtil;
-import io.smallrye.config.ConfigValidationException;
-import sun.misc.Signal;
-import sun.misc.SignalHandler;
+import static java.lang.Boolean.parseBoolean;
 
 /**
  * Manages the lifecycle of a Quarkus application.
@@ -228,7 +228,6 @@ public class ApplicationLifecycleManager {
                     Runtime.getRuntime().removeShutdownHook(sh);
                 }
             } catch (IllegalStateException ignore) {
-
             }
         }
         if (!alreadyStarted) {
@@ -481,6 +480,6 @@ public class ApplicationLifecycleManager {
     }
 
     public static boolean isAppCDSGeneration() {
-        return Boolean.parseBoolean(System.getProperty(ApplicationLifecycleManager.QUARKUS_APPCDS_GENERATE_PROP, "false"));
+        return parseBoolean(System.getProperty(QUARKUS_APPCDS_GENERATE_PROP, "false"));
     }
 }
