4b7db379c7606670c1943bb04715b996181cf063
Vincent Potucek <vincent.potucek@udg.de>
2025-01-07 15:42:15 +0100

    wip

--- core/runtime/src/main/java/io/quarkus/runtime/metrics/MetricsFactory.java
+++ core/runtime/src/main/java/io/quarkus/runtime/metrics/MetricsFactory.java
@@ -13,10 +13,10 @@ import java.util.function.Supplier;
 public interface MetricsFactory {
 
     /** A well-known string for MicroProfile metrics provided by the SmallRye Metrics quarkus extension */
-    final String MP_METRICS = "smallrye-metrics";
+    String MP_METRICS = "smallrye-metrics";
 
     /** A well-known string for Micrometer metrics provided by the Micrometer Metrics quarkus extension */
-    final String MICROMETER = "micrometer";
+    String MICROMETER = "micrometer";
 
     /** Registry type or scope. This may not be used by all metrics extensions. */
     public static enum Type {
